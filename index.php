4680c8de909e8b66c234e340af55c84a896a54c2
joachimjamelo <98930194+joachimjamelo@users.noreply.github.com>
2022-02-24 11:56:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <body background="file:///C:/Users/iWarehouse/OneDrive/Desktop/AdobeStock_364791487-2000x1000.jpeg">
 <font color=white>
-<h1 align =center>WEB PAGE</h1>
+<h1> align =center>WEB PAGE</h1>
 </font>
 <hr>
 <center>
